111dee7f640516a8fa3c2704c38a5b79cdda90a6
Donghak Kim <119437754+enbraining@users.noreply.github.com>
2024-11-05 16:08:52 +0900

    Create 합 구하기.cpp

---
+++ Baekjoon/합 구하기.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    int n_array[n];
+
+    for(int x = 0; x < n; x++) {
+        cin >> n_array[x];
+    }
+
+    for(int x = 1; x < n; x++) {
+        n_array[x] = n_array[x - 1] + n_array[x];
+    }
+
+    int m;
+    cin >> m;
+    for(int x = 0; x < m; x++) {
+        int i, j;
+        cin >> i >> j;
+
+        if(i == 1) cout << n_array[j - 1] << '\n';
+        else cout << n_array[j - 1] - n_array[i - 2] << '\n';
+    }
+}
